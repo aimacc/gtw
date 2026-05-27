ccbfd2062648184f8b43358a1deacaa7200edc07
aimacc <aimalopedapa@gmail.com>
2026-05-27 16:01:52 +0700

    Update orderkuota.js
    Signed-off-by: aimacc <aimalopedapa@gmail.com>

--- src/lib/orderkuota.js
+++ src/lib/orderkuota.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+const axios = require('axios')
 
 const API_URL = 'https://app.orderkuota.com:443/api/v2'
 
@@ -12,7 +12,8 @@ const APP_VERSION_NAME = '25.08.11'
 const APP_VERSION_CODE = '250811'
 const APP_REG_ID = 'di309HvATsaiCppl5eDpoc:APA91bFUcTOH8h2XHdPRz2qQ5Bezn-3_TaycFcJ5pNLGWpmaxheQP9Ri0E56wLHz0_b1vcss55jbRQXZgc9loSfBdNa5nZJZVMlk7GS1JDMGyFUVvpcwXbMDg8tjKGZAurCGR4kDMDRJ'
 
-export async function loginRequest(username, password) {
+async function loginRequest(username, password) {
+
   const payload = new URLSearchParams({
     username,
     password,
@@ -28,36 +29,6 @@ export async function loginRequest(username, password) {
   return data
 }
 
-export async function getAuthToken(username, otp) {
-  const payload = new URLSearchParams({
-    username,
-    password: otp,
-    app_reg_id: APP_REG_ID,
-    app_version_code: APP_VERSION_CODE,
-    app_version_name: APP_VERSION_NAME
-  })
-
-  const { data } = await axios.post(`${API_URL}/login`, payload, {
-    headers
-  })
-
-  return data
-}
-
-export async function getQris(username, token) {
-  const payload = new URLSearchParams({
-    request_time: Math.floor(Date.now() / 1000),
-    auth_username: username,
-    auth_token: token,
-    app_reg_id: APP_REG_ID,
-    app_version_code: APP_VERSION_CODE,
-    app_version_name: APP_VERSION_NAME,
-    'requests[0]': 'account'
-  })
-
-  const { data } = await axios.post(`${API_URL}/get`, payload, {
-    headers
-  })
-
-  return data
+module.exports = {
+  loginRequest
 }
