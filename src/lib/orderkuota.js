a6dc857963fbb7e0ac3528801e355688257e6a4b
aimacc <aimalopedapa@gmail.com>
2026-05-27 16:05:34 +0700

    Update orderkuota.js
    Signed-off-by: aimacc <aimalopedapa@gmail.com>

--- src/lib/orderkuota.js
+++ src/lib/orderkuota.js
@@ -2,31 +2,59 @@
 
 const API_URL = 'https://app.orderkuota.com:443/api/v2'
 
-const headers = {
-  Host: 'app.orderkuota.com',
-  'User-Agent': 'okhttp/4.12.0',
-  'Content-Type': 'application/x-www-form-urlencoded'
-}
-
 const APP_VERSION_NAME = '25.08.11'
 const APP_VERSION_CODE = '250811'
-const APP_REG_ID = 'di309HvATsaiCppl5eDpoc:APA91bFUcTOH8h2XHdPRz2qQ5Bezn-3_TaycFcJ5pNLGWpmaxheQP9Ri0E56wLHz0_b1vcss55jbRQXZgc9loSfBdNa5nZJZVMlk7GS1JDMGyFUVvpcwXbMDg8tjKGZAurCGR4kDMDRJ'
+
+const headers = {
+  Host: 'app.orderkuota.com',
+  Connection: 'Keep-Alive',
+  Accept: 'application/json',
+  'Accept-Encoding': 'gzip',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent': 'okhttp/4.12.0'
+}
 
 async function loginRequest(username, password) {
 
   const payload = new URLSearchParams({
     username,
     password,
-    app_reg_id: APP_REG_ID,
+    app_reg_id: 'di309HvATsaiCppl5eDpoc',
     app_version_code: APP_VERSION_CODE,
-    app_version_name: APP_VERSION_NAME
+    app_version_name: APP_VERSION_NAME,
+    phone_model: 'SM-G960N',
+    phone_android_version: '9',
+    ui_mode: 'light',
+    request_time: Math.floor(Date.now() / 1000)
   })
 
-  const { data } = await axios.post(`${API_URL}/login`, payload, {
-    headers
-  })
+  try {
 
-  return data
+    const { data } = await axios({
+      method: 'POST',
+      url: `${API_URL}/login`,
+      headers,
+      data: payload.toString()
+    })
+
+    return data
+
+  } catch (err) {
+
+    if (err.response) {
+      return {
+        status: false,
+        code: err.response.status,
+        data: err.response.data
+      }
+    }
+
+    return {
+      status: false,
+      error: err.message
+    }
+  }
+
 }
 
 module.exports = {
